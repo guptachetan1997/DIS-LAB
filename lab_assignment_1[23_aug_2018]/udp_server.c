a0fa89272f7f90a6e2f7992b0a88f651d74eefde
Chetan <gupta.chetan1997@gmail.com>
2018-08-30 12:00:23 +0530

    LAB 2

--- lab_assignment_1[23_aug_2018]/udp_server.c
+++ lab_assignment_1[23_aug_2018]/udp_server.c
@@ -1,3 +1,4 @@
+// UDP Server - Listens to client request and sends the server date time.
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,53 +9,35 @@
 #include <netinet/in.h>
 #include <time.h>
  
-#define PORT     8080
+#define PORT 8081
 #define MAXLINE 1024
  
 int main() {
 	int sockfd;
 	char buffer[MAXLINE];
-	// char *hello = "23 Aug 2018";
 	time_t t = time(NULL);
     struct tm *tm = localtime(&t);
     char *hello = asctime(tm);
-
 	struct sockaddr_in servaddr, cliaddr;
-	 
-	// Creating socket file descriptor
 	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-	 
 	memset(&servaddr, 0, sizeof(servaddr));
 	memset(&cliaddr, 0, sizeof(cliaddr));
-	 
-	// Filling server information
 	servaddr.sin_family    = AF_INET; // IPv4
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 	servaddr.sin_port = htons(PORT);
-	 
-	// Bind the socket with the server address
-	if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
-			sizeof(servaddr)) < 0 )
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 )
 	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
-	 
-	int len, n, counts=0;
-	while(1){
-		n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-					MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-					&len);
-		buffer[n] = '\0';
-		printf("Client : %s\n", buffer);
-		sendto(sockfd, (const char *)hello, strlen(hello), 
-			MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-				len);
-		printf("#%d == Date data sent.\n", counts);
-		counts++;
-	}
+	int len, n;
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr,&len);
+	buffer[n] = '\0';
+	printf("Client : %s\n", buffer);
+	sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *) &cliaddr,len);
+	close(sockfd);
 	return 0;
 }
---
+++ lab_assignment_1[30_aug_2018]/coordinator.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+ 
+#define PORT 8000
+#define MAXLINE 1024
+#define RESOURCES 2
+#define MAX_QUEUE_SIZE 10
+
+struct sockaddr_in cs_queue[RESOURCES][MAX_QUEUE_SIZE], resource_holder[RESOURCES];
+int queue_front[RESOURCES]={0}, queue_back[RESOURCES]={0};
+
+void enqueue(struct sockaddr_in client, int resource_id)
+{
+	if(queue_back[resource_id] == MAX_QUEUE_SIZE)
+	{
+		printf("Queue is full\n");
+		return;
+	}
+	cs_queue[resource_id][queue_back[resource_id]] = client;
+	queue_back[resource_id]++;
+}
+
+struct sockaddr_in dequeue(int resource_id)
+{
+	struct sockaddr_in top;
+	if(queue_back == queue_front)
+	{
+		printf("Queue is empty");
+		return top;
+	}
+	top = cs_queue[resource_id][queue_front[resource_id]];
+	queue_front[resource_id]++;
+	return top;
+}
+
+int queue_empty(int resource_id)
+{
+	if(queue_back[resource_id] == queue_front[resource_id])
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int create_connection()
+{
+    int sockfd;
+	struct sockaddr_in servaddr;
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	int optval = 1;
+  	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family    = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(PORT);
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+    return sockfd;
+}
+ 
+int main() {
+	
+    char buffer[MAXLINE];
+	int CS_MUTEX[RESOURCES] = {0};
+    int sockfd = create_connection();
+	struct sockaddr_in cliaddr;
+	int len, n;
+	while(1)
+	{
+		memset(&cliaddr, 0, sizeof(cliaddr));
+		n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+		buffer[n] = '\0';
+		char response[MAXLINE];
+		printf("%d : %s \n", cliaddr.sin_port, buffer);
+		if(!strcmp(buffer, "PING"))
+		{
+			strcpy(response, "PONG");
+		}
+		else if(!strncmp(buffer, "REQUEST", 7))
+		{
+			int resource_id = buffer[8] - '0';
+			if(CS_MUTEX[resource_id] == 0)
+			{
+				CS_MUTEX[resource_id] = 1;
+				strcpy(response, "GRANTED");
+				resource_holder[resource_id] = cliaddr;
+			}
+			else
+			{
+				strcpy(response, "UP");
+				enqueue(cliaddr, resource_id);
+				strcpy(response, "DENIED");
+			}
+		}
+		else if(!strncmp(buffer, "RELEASE", 7))
+		{
+			int resource_id = buffer[8] - '0';
+			if(queue_empty(resource_id))
+			{
+				CS_MUTEX[resource_id] = 0;
+			}
+			else{
+				struct sockaddr_in queue_top = dequeue(resource_id);
+				strcpy(response, "GRANTED");
+				sendto(sockfd, (const char *)response, strlen(response), MSG_CONFIRM, (const struct sockaddr *) &queue_top, len);
+				resource_holder[resource_id] = queue_top;
+			}
+			strcpy(response, "ACK");
+		}
+		else
+		{
+			strcpy(response, "ERROR");
+		}
+		sendto(sockfd, (const char *)response, strlen(response), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
+	}
+	close(sockfd);
+	return 0;
+}
