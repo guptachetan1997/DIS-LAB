8737b69db8ad6129c7cb26bff6bc24e9e043bedc
Chetan <gupta.chetan1997@gmail.com>
2018-10-25 11:39:25 +0530

    lab_7

--- lab_7/client.c
+++ lab_7/client.c
@@ -1,4 +1,4 @@
-// Client service for mutual exclusion
+// Central time coordinator for Berkeley time synch
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,48 +7,95 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
- 
-#define PORT 8000
+#include <netdb.h>
+#include <time.h>
+#include <sys/poll.h>
+
 #define MAXLINE 1024
- 
-// Driver code
-int main(int argc, char* argv[]) {
+#define MAX_PROCESS 5
+
+
+int max(int a , int b)
+{
+    if(a > b)
+        return a;
+    else
+        return b;
+}
+
+struct Message
+{
+	char type; // P->poll for local time, S->synch time, R -> reply for time poll
+	int timestamp;
+	int change;
+	
+};
+
+int create_connection(int PORT)
+{
     int sockfd;
-    char buffer[MAXLINE];
-    struct sockaddr_in servaddr;
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-    int optval = 1;
+	struct sockaddr_in servaddr;
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	int optval = 1;
   	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(PORT);
-    int n, len;
-    char *hello = "PING", *request, *release="RELEASE";
-    request = malloc(strlen(argv[1]) + 8);
-    strcpy(request, "REQUEST_");
-    strcat(request, argv[1]);
-    sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-    buffer[n] = '\0';
-    if(!strcmp(buffer, "PONG"))
-    {
-        sendto(sockfd, (const char *)request, strlen(request), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-        n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-        buffer[n] = '\0';
-        if(!strcmp(buffer, "DENIED"))
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family    = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(PORT);
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+    return sockfd;
+}
+
+void send_message(int dest_port, int sockfd, struct Message newMsg)
+{
+	struct sockaddr_in client_addr;
+	memset(&client_addr, 0, sizeof(client_addr));
+	client_addr.sin_family    = AF_INET; // IPv4
+	client_addr.sin_addr.s_addr = INADDR_ANY;
+	client_addr.sin_port = htons(dest_port);
+    sendto(sockfd, (struct Message*)&newMsg, (1024+sizeof(newMsg)), 0, (struct sockaddr *) &client_addr, sizeof(client_addr));
+}
+
+int main(int argc, char* argv[]) {
+    srand ( time(NULL) );
+    int local_time = 100 - rand()%20;
+	int MY_PORT= atoi(argv[1]);
+	int TIME_SERVER_PORT= atoi(argv[2]);
+
+    struct Message * temp = malloc(sizeof(struct Message));
+
+	printf("Initialising the time server at port %d [local time : %d].\n", MY_PORT, local_time);
+    int sockfd = create_connection(MY_PORT);
+
+    struct sockaddr_in recv_client_addr, send_client_addr;
+    int len, n;
+
+    while(1){
+        n = recvfrom(sockfd, temp, sizeof(*temp), MSG_WAITALL, ( struct sockaddr *) &recv_client_addr, &len);
+        struct Message newMsg;
+        if(temp->type == 'P')
         {
-            printf("Request denied\n");
-            n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-            buffer[n] = '\0';
+            printf("Server sent a time poll message.\n");
+            newMsg.type = 'R';
+            newMsg.timestamp = local_time;
+            newMsg.change = 0;
+            send_message(TIME_SERVER_PORT, sockfd, newMsg);
         }
-        printf("Performing critical section\n");
-        sleep(10);
-        sendto(sockfd, (const char *)release, strlen(release), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+        else if(temp->type == 'S')
+        {
+            printf("New Synch time recieved from server : %d\n", temp->timestamp);
+            if(temp->change < 0)
+                printf("Set clock ahead by %d\n", -temp->change);
+            else
+                printf("Set clock slow by %d\n", temp->change);
+        }
     }
-    close(sockfd);
-    return 0;
 }
--- lab_7/time_coordinator.c
+++ lab_7/time_coordinator.c
@@ -1,4 +1,4 @@
-// Central time coordinator for berkeley time synch
+// Central time coordinator for Berkeley time synch
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -25,7 +25,7 @@ int max(int a , int b)
 
 struct Message
 {
-	char type; // P->poll for local time, S->synch time
+	char type; // P->poll for local time, S->synch time, R -> reply for time poll
 	int timestamp;
 	int change;
 	
@@ -54,39 +54,61 @@ int create_connection(int PORT)
     return sockfd;
 }
 
-int send_time_poll_message(int sockfd, int OTHER_PROCESS_PORTS[], int NUM_PROCESSES, int MY_PORT)
+void send_message(int dest_port, int sockfd, struct Message newMsg, struct sockaddr_in client)
 {
-	int i;
-	char response[MAXLINE];
-	strcpy(response, "ELEC");
-	int flag=1;
-	for(i=0 ; i<NUM_PROCESSES ; i++)
-	{
-	    send_client_addr.sin_family = AF_INET;
-	    send_client_addr.sin_addr.s_addr = INADDR_ANY;
-	    send_client_addr.sin_port   = htons(OTHER_PROCESS_PORTS[i]);
-		if(OTHER_PROCESS_PORTS[i] > MY_PORT)
-		{
-			send_message(OTHER_PROCESS_PORTS[i], sockfd, response);
-			flag=0;
-		}
-	}
-	return flag;
+    struct sockaddr_in client_addr;
+    if(dest_port != -1)
+    {
+        memset(&client_addr, 0, sizeof(client_addr));
+	    client_addr.sin_family    = AF_INET; // IPv4
+	    client_addr.sin_addr.s_addr = INADDR_ANY;
+	    client_addr.sin_port = htons(dest_port);
+    }
+    else
+    {
+        client_addr = client;
+    }
+    sendto(sockfd, (struct Message*)&newMsg, (1024+sizeof(newMsg)), 0, (struct sockaddr *) &client_addr, sizeof(client_addr));
 }
 
-int send_time_synch_message(int sockfd, int OTHER_PROCESS_PORTS[], int NUM_PROCESSES, int MY_PORT)
+int send_time_poll_message(int sockfd, int OTHER_PROCESS_PORTS[], int NUM_PROCESSES, int MY_PORT)
 {
-	int i;
-	char response[MAXLINE];
-	strcpy(response, "ELEC");
-	int flag=1;
+    printf("Starting the Berkeley Time Snchronization \n");
+    struct sockaddr_in recv_client_addr[NUM_PROCESSES], send_client_addr;
+    struct Message * temp = malloc(sizeof(struct Message));
+    int len=sizeof(struct sockaddr_in), n, i, flag=1;
+    struct Message newMsg;
+    newMsg.type = 'P';
+    newMsg.timestamp = -1;
+    newMsg.change = 0;
 	for(i=0 ; i<NUM_PROCESSES ; i++)
 	{
-		if(OTHER_PROCESS_PORTS[i] > MY_PORT)
-		{
-			send_message(OTHER_PROCESS_PORTS[i], sockfd, response);
-			flag=0;
-		}
+		send_message(OTHER_PROCESS_PORTS[i], sockfd, newMsg, send_client_addr);
+	}
+    int total_time_sum=0, local_client_times[NUM_PROCESSES];
+    for(i=0 ; i<NUM_PROCESSES ; i++)
+	{
+        n = recvfrom(sockfd, temp, sizeof(*temp), MSG_WAITALL, ( struct sockaddr *) &recv_client_addr[i], &len);
+        if(temp->type == 'R')
+        {
+
+            local_client_times[i] = temp->timestamp;
+            total_time_sum += temp->timestamp;
+            printf("Local time of process %d : %d\n", ntohs(recv_client_addr[i].sin_port), temp->timestamp);
+        }
+        else
+        {
+            printf("Error occured\n");
+            exit(0);
+        }
+	}
+    printf("New Synch Time : %d \n", total_time_sum/NUM_PROCESSES);
+	for(i=0 ; i<NUM_PROCESSES ; i++)
+	{
+        newMsg.type = 'S';
+        newMsg.timestamp = total_time_sum/NUM_PROCESSES;
+        newMsg.change = local_client_times[i] - (total_time_sum/NUM_PROCESSES);
+		send_message(-1, sockfd, newMsg, recv_client_addr[i]);
 	}
 	return flag;
 }
@@ -113,66 +135,5 @@ int main(int argc, char* argv[]) {
 
     char response[MAXLINE];
 
-    if(IS_INITIATOR)
-    {
-        struct Message newMsg;
-        newMsg.point = 0;
-        newMsg.COORD = MY_PORT;
-        newMsg.type = 'E';
-        newMsg.participants[newMsg.point] = MY_PORT;
-        sendto(sockfd, (struct Message*)&newMsg, (1024+sizeof(newMsg)), 0, (struct sockaddr *)
-         &send_client_addr, sizeof(send_client_addr));
-        ELECTION_FLAG=1;
-	}
-    while(1)
-    	{
-	    	// memset(&recv_client_addr, 0, sizeof(recv_client_addr));
-            struct Message newMsg;
-			n = recvfrom(sockfd, temp, sizeof(*temp), MSG_WAITALL, ( struct sockaddr *) &recv_client_addr, &len);
-            switch(temp->type)
-            {
-                case 'E':
-                    printf("Msg Recvd : ELECTION {");
-                    if (ELECTION_FLAG)
-                    {
-                        printf("}\n");
-                        newMsg.type = 'C';
-                        newMsg.COORD = temp->COORD;
-                        newMsg.point = temp->point;
-                        for(int i=0 ; i<=temp->point ; i++)
-                            newMsg.participants[i] = temp->participants[i];
-                        for(int i=0 ; i<=temp->point ; i++){
-                            send_client_addr.sin_family = AF_INET;
-                            send_client_addr.sin_addr.s_addr = INADDR_ANY;
-                            send_client_addr.sin_port   = htons(temp->participants[i]);
-                            sendto(sockfd, (struct Message*)&newMsg, (1024+sizeof(newMsg)), 0, (struct sockaddr *)
-                             &send_client_addr, sizeof(send_client_addr));
-                        }
-                    }
-                    else
-                    {
-                        newMsg.point = temp->point+1;
-                        newMsg.type = 'E';
-                        newMsg.COORD = max(temp->COORD, MY_PORT);
-                        for(int i=0 ; i<=temp->point ; i++)
-                        {
-                            printf("%d,", temp->participants[i]);
-                            newMsg.participants[i] = temp->participants[i];
-                        }
-                        printf("}\n");
-                        newMsg.participants[newMsg.point] = MY_PORT;
-                        sendto(sockfd, (struct Message*)&newMsg, (1024+sizeof(newMsg)), 0, (struct sockaddr *)
-                         &send_client_addr, sizeof(send_client_addr));
-                         ELECTION_FLAG=1;
-                    }
-                    break;
-                case 'A':
-                    printf("Msg Recvd : Ack\n");
-                    break;
-                case 'C':
-                    printf("Msg Recvd : COORDINATOR\n");
-                    printf("New COORDINATOR is : %d\n", temp->COORD);
-                    break;
-            };
-		}
+    send_time_poll_message(sockfd, OTHER_PROCESS_PORTS, NUM_PROCESSES, MY_PORT);
 }
